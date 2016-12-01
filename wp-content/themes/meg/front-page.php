6ca33045f3891d7541b98ed111ceecc3129cd6c0
aumdove <aumdove13@gmail.com>
2016-11-30 18:14:48 -1000

    sorta lost as to why these custom post types arent showing up as expected on home page...

--- wp-content/themes/meg/front-page.php
+++ wp-content/themes/meg/front-page.php
@@ -10,17 +10,36 @@
 
 get_header(); ?>
 
-    <div id="primary" class="content-area">
-        <main id="main" class="site-main" role="main">
-                <div class="front-page">
-			<?php while ( have_posts() ): the_post(); ?>
-			<h1><?php the_title(); ?></h1>
-                        <p><?php the_content() ?></p>
-			<?php endwhile; ?>
-		</div>
-        </main><!-- #main -->
-    </div><!-- #primary -->
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+        
+        <?php while ( have_posts() ) : the_post(); ?>
+            <?php the_content(); ?>
+        <?php endwhile; // end of the loop. ?>
+        
+        
+        <div class="featured-work-front">
+            <h4>Featured Work</h4> 
+            <ul class="frontpage-work-list">
+            <?php query_posts('posts_per_page=3&post_type=featured_work'); ?>
+                <?php while ( have_posts() ): the_post();
+                    $image_1 = get_field("image_1");
+                    $size = "large";
+                ?>
+                <li class="individual-featured-work">
+                    <figure>
+                        <?php echo wp_get_attachment_image($image_1, $size); ?>
+                    </figure>
+                    <h3><a href="<?php the_permalink(); ?>"<?php the_title(); ?></a></h3>
+                </li>
+                <?php endwhile; //end of the loop. ?>
+                <?php wp_reset_query(); //resets the altered query back to original. ?>
+            </ul>
 
+        </div>
+            
+    </main><!-- #main -->
+</div><!-- #primary -->
 <?php
 get_sidebar();
 get_footer();
