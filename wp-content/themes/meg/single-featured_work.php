7951176d79ebd35234127ac7c0fe1f3300fe76ad
aumdove <aumdove13@gmail.com>
2016-11-30 18:17:48 -1000

    fix merge conflict

--- wp-content/themes/meg/single-featured_work.php
+++ wp-content/themes/meg/single-featured_work.php
@@ -12,44 +12,42 @@ get_header(); ?>
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
 
-        <?php while ( have_posts() ) : the_post(); 
 
-            $work = get_field('work');
-            $client = get_field('client');
-            $link = get_field('live_site_link');
-            $image_1 = get_field('image_1');
-            $image_2 = get_field('image_2');
-            $image_3 = get_field('image_3');
+    <?php while ( have_posts() ) : the_post(); 
+            
+        $work = get_field('work');
+        $client = get_field('client');
+        $link = get_field('live_site_link');
+        $image_1 = get_field('image_1');
+        $image_2 = get_field('image_2');
+        $image_3 = get_field('image_3');
+    
+    ?>
+    <div class="featured-work">   
+        <div class="featured-work-sidebar">
+            <h2><?php the_title(); ?></h2>
+            <h5><?php echo $work; ?></h5>
+            <h6>Client: <?php echo $client; ?></h6>
 
-        ?>
-        <div class="featured-work-content" >   
-            <div class="featured-work-sidebar">
-                <h2><?php the_title(); ?></h2>
-                <h5><?php echo $work; ?></h5>
-                <h6>Client: <?php echo $client; ?></h6>
+            <?php the_content(); ?>
+            
+            <p><a href="<?php echo $link; ?>" target="_blank">Live Site Link</a></p>
 
-                <?php the_content(); ?>
+        </div>
 
-                <p><a href="<?php echo $link; ?>" target="_blank">Live Site Link</a></p>
+        <div class="featured-work-images">
+            <?php if($image_1){ ?>
+                <img src="<?php echo $image_1; ?>" />
+            <?php } ?>
+            <?php if($image_2){ ?>
+                <img src="<?php echo $image_2; ?>" />
+            <?php } ?>
+            <?php if($image_3){ ?>
+                <img src="<?php echo $image_3; ?>" />
+            <?php } ?>
+        </div>
 
-            </div>
-
-            <div class="featured-work-image">
-
-                <?php if($image_1){ ?>
-                    <img src="<?php echo $image_1; ?>" />
-                <?php } ?>
-                <?php if($image_2){ ?>
-                    <img src="<?php echo $image_2; ?>" />
-                <?php } ?>
-                <?php if($image_3){ ?>
-                    <img src="<?php echo $image_3; ?>" />
-                <?php } ?>
-            </div>
-
-
-        </div> 
-
+    </div> 
 <?php endwhile; // End of the loop. ?>
 
         </main><!-- #main -->
